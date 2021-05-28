91b2c9a0530fffc83ec93f16f0bc5219fa4267a4
Kenny Poncio <kenpoc4@gmail.com>
2021-05-27 21:50:18 -0600

    Nuevos cambios

--- functions.php
+++ functions.php
@@ -8,6 +8,7 @@ function no_importa_el_nombre_de_esta_funcion(){
     
     // Registro de fuentes
     wp_enqueue_style( 'fuente-open', 'https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;600;700&display=swap', array(), '1.0.0');
+    wp_enqueue_style( 'fuente-monse', 'https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400&display=swap', array(), '1.0.0' );
 
     // Registro de archivo JS
     wp_enqueue_script( 'scripts', get_template_directory_uri() . '/js/scripts.js', array( 'jquery' ), '1.0.0', true);
